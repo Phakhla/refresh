358c0208d62712961aa8c58342e1fcb9193193d6
Phakhla <worrapon1122@gmail.com>
2021-03-05 14:47:19 +0700

    refresh

---
+++ plugins/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpQiH1Nu_7SqZ017voOge7X4Vf7c7aXSA",
+    authDomain: "rtapp-fe9f9.firebaseapp.com",
+    projectId: "rtapp-fe9f9",
+    storageBucket: "rtapp-fe9f9.appspot.com",
+    messagingSenderId: "106517579811",
+    appId: "1:106517579811:web:91ae538da68efdeb38c996"
+  };
